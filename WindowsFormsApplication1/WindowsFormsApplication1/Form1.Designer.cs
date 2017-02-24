5bebf7605b42638163d3815cb4011848cda304c9
akp4657 <akp4657@IGMOH1380-01>
2017-02-24 15:07:32 -0500

    GUI Framework

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class externalEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapPiece = new System.Windows.Forms.PictureBox();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mapPiece)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapPiece
+            // 
+            this.mapPiece.Location = new System.Drawing.Point(190, 115);
+            this.mapPiece.Name = "mapPiece";
+            this.mapPiece.Size = new System.Drawing.Size(311, 310);
+            this.mapPiece.TabIndex = 0;
+            this.mapPiece.TabStop = false;
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(537, 272);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(75, 23);
+            this.nextBtn.TabIndex = 1;
+            this.nextBtn.Text = "button1";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            // 
+            // backBtn
+            // 
+            this.backBtn.Location = new System.Drawing.Point(72, 272);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(75, 23);
+            this.backBtn.TabIndex = 2;
+            this.backBtn.Text = "button2";
+            this.backBtn.UseVisualStyleBackColor = true;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Location = new System.Drawing.Point(331, 75);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(35, 13);
+            this.title.TabIndex = 3;
+            this.title.Text = "label1";
+            // 
+            // externalEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 591);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.mapPiece);
+            this.Name = "externalEditor";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mapPiece)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mapPiece;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Label title;
+    }
+}
+
